414583c4ec2891770cbaa853527deab761500263
Corban Swain <nabsterrock@gmail.com>
2017-03-14 21:01:24 -0500

    Setting up Storyboard

---
+++ SpectroApp_V1/SpectroApp_V1/ProjectTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProjectTableViewCell.swift
+//  SpectroApp_V1
+//
+//  Created by Corban Swain on 3/14/17.
+//  Copyright © 2017 CorbanSwain. All rights reserved.
+//
+
+import UIKit
+
+class ProjectTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
