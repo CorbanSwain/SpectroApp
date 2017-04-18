a7b7f2f74124c6d43cd92ed880f80b2a4ea0e105
Mary Richardson <smarmey@me.com>
2017-04-17 21:11:16 -0500

    Working on export popover

--- SpectroApp_V1/SpectroApp_V1/ExportPopoverViewController.swift
+++ SpectroApp_V1/SpectroApp_V1/ExportPopoverViewController.swift
@@ -9,6 +9,58 @@
 import UIKit
 
 class ExportPopoverViewController: UIViewController {
+    
+    
+    
+    
+    
+    func writeDataToFile(file:String)-> Bool{
+        // store the data to export
+        let data = "Testing"
+        
+        // get the file path for the file in the bundle
+        // if it doesn't exist, make it in the bundle
+        let fileName = file + ".txt"
+        
+        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+            let path = dir.appendingPathComponent(fileName)
+            
+            do {
+                try data.write(to: path, atomically: false, encoding: String.Encoding.utf8)
+                return true
+            }
+            catch {
+                print("could not write to file")
+                return false
+            }
+        }
+        return false
+    }
+    
+        /*
+        if let filePath = Bundle.main.path(forResource: file, ofType: "txt"){
+            fileName = filePath
+        } else {
+            fileName = Bundle.main.bundlePath + fileName
+        }
+        // write the file, return true if it works, false otherwise
+        do {
+            try data.write(toFile: fileName, atomically: true, encoding: String.Encoding.utf8)
+            return true
+        } catch{
+            return false
+        }
+        */
+    
+    
+    @IBAction func writeData(sender: UIButton) {
+        if writeDataToFile(file: "data") {
+            print("data written")
+        } else {
+            print("data not written")
+        }
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
