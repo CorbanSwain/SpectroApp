7e28cc3fe79551ef0cb4adba3b133b86300d0342
Mary Richardson <smarmey@me.com>
2017-04-17 23:05:29 -0500

    Added open with popup for export

--- SpectroApp_V1/SpectroApp_V1/ExportPopoverViewController.swift
+++ SpectroApp_V1/SpectroApp_V1/ExportPopoverViewController.swift
@@ -8,15 +8,29 @@
 
 import UIKit
 
-class ExportPopoverViewController: UIViewController {
+class ExportPopoverViewController: UIViewController, UIDocumentInteractionControllerDelegate {
     
+    @IBOutlet weak var fileNameText: UITextField!
     
+    var documentController : UIDocumentInteractionController!
     
+    var project: Project!
     
     
     func writeDataToFile(file:String)-> Bool{
         // store the data to export
-        let data = "Testing"
+        
+        let readings = project.readingArray
+        var data = project.title + "\n"
+        for (i,reading) in readings.enumerated() {
+            if let value = reading.absorbanceValue {
+                data.append(Formatter.threeDecNum.string(from: value as NSNumber)!)
+                if i < readings.count - 1 {
+                    data.append(",")
+                }
+            }
+        }
+        print(data)
         
         // get the file path for the file in the bundle
         // if it doesn't exist, make it in the bundle
@@ -24,6 +38,8 @@ class ExportPopoverViewController: UIViewController {
         
         if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
             let path = dir.appendingPathComponent(fileName)
+            
+            documentController = UIDocumentInteractionController(url: path)
             
             do {
                 try data.write(to: path, atomically: false, encoding: String.Encoding.utf8)
@@ -33,35 +49,22 @@ class ExportPopoverViewController: UIViewController {
                 print("could not write to file")
                 return false
             }
+            
         }
         return false
     }
     
-        /*
-        if let filePath = Bundle.main.path(forResource: file, ofType: "txt"){
-            fileName = filePath
-        } else {
-            fileName = Bundle.main.bundlePath + fileName
-        }
-        // write the file, return true if it works, false otherwise
-        do {
-            try data.write(toFile: fileName, atomically: true, encoding: String.Encoding.utf8)
-            return true
-        } catch{
-            return false
-        }
-        */
-    
     
     @IBAction func writeData(sender: UIButton) {
-        if writeDataToFile(file: "data") {
+        if writeDataToFile(file: fileNameText.text!) {
             print("data written")
         } else {
             print("data not written")
         }
+        documentController.presentOptionsMenu(from: sender.frame, in: self.view, animated:true)
     }
 
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
