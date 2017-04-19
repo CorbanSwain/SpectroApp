2c2a827684b2de3efdab0541b290f181e23a9320
Mary Richardson <smarmey@me.com>
2017-04-19 13:04:00 -0500

    Working on notes view

---
+++ SpectroApp_V1/SpectroApp_V1/AddProjectNotesTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  AddProjectNotesTableViewCell.swift
+//  SpectroApp_V1
+//
+//  Created by Mary Richardson on 4/19/17.
+//  Copyright © 2017 CorbanSwain. All rights reserved.
+//
+
+import UIKit
+
+class AddProjectNotesTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var textInput: UITextView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
