49dc080d27cd5126ba1c76d24eb722446e6c2699
Corban Swain <CorbanSwain@gmail.com>
2017-04-20 19:00:39 -0500

    changes to info info menu, fixing database and auto layout bugs

--- SpectroApp_V1/SpectroApp_V1/ExperimentHeaderView.swift
+++ SpectroApp_V1/SpectroApp_V1/ExperimentHeaderView.swift
@@ -10,14 +10,13 @@ import UIKit
 
 class ExperimentHeaderView: UIView {
 
-    @IBOutlet weak var mainLabel: UILabel!
     @IBOutlet weak var subLabel: UILabel!
     @IBOutlet weak var backgroundAccent: UIView!
     @IBOutlet weak var titleField: UITextField!
     
     var mainText: String! {
         didSet {
-            mainLabel.text = mainText ?? "Untitled"
+            titleField.text = mainText ?? "Untitled"
         }
     }
     
--- SpectroApp_V1/SpectroApp_V1/MasterNavigationBar.swift
+++ SpectroApp_V1/SpectroApp_V1/MasterNavigationBar.swift
@@ -12,11 +12,16 @@ class MasterNavigationBar: UINavigationBar {
 
     
     override func sizeThatFits(_ size: CGSize) -> CGSize {
-        return CGSize(width: (superview?.frame.width)!, height: 55)
+//        print("resizing navbar! \n\t↳ MasterNavBar.sizeThatFits")
+        super.sizeThatFits(size)
+//        print("just finished super implementation ... resizing navbar! \n\t↳ MasterNavBar.sizeThatFits")
+//        print("returning a size: {\((superview?.frame.width)!),55}  \n\t↳ MasterNavBar.sizeThatFits")
+        return CGSize(width: (superview?.frame.width)!, height: 58)
     }
     
     override func layoutSubviews() {
-         super.layoutSubviews()
+//         print("laying out subviews! \n\t↳ MasterNavBar.layoutSubviews")
+        super.layoutSubviews()
         setTitleVerticalPositionAdjustment(-2, for: .default)
     }
 
